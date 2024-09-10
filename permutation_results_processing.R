1c96423d044b03d98f146394a993b20427457e07
Kelly Mistry <kelly.r.mistry@gmail.com>
2024-09-10 11:43:44 -0700

    Cleaning up script

--- permutation_results_processing.R
+++ permutation_results_processing.R
@@ -9,7 +9,18 @@ source(here("Scripts/model_evaluation.R"))
 source(here("Scripts/results_processing_functions.R"))
 source(here("Scripts/alt_strategies_cost_calc.R"))
 
-# Plot labels 
+# Strategy  & permutation names
+strategies <- paste0("Strategy_", c("one", "two", "three", "four"))
+permutations <- vector()
+counter <- 1
+for(p in 1:length(starting_pop)) {
+  for(d in 1:length(starting_size_dist)) {
+    permutations[counter] <- paste0(names(starting_pop)[p], "_", names(starting_size_dist)[d])
+    counter <- counter + 1
+  }
+}
+
+# Plot labels & colors
 plot_labels <- list()
 plot_labels$size_class <- c("small" = "Small",
                             "medium" = "Medium",
@@ -24,137 +35,29 @@ plot_labels$type_of_N <- c("small" = "Small",
                            "large" = "Large", 
                            "xlarge" = "X-large", 
                            "total" = "Total")
+plot_labels$strategy <- c("ADS only",
+                          "all methods",
+                          "ground-based methods",
+                          "ADS plus monitoring")
+names(plot_labels$strategy) <- strategies
+plot_labels$permutation <- c("low density,\n more small snakes",
+                            "low density,\n more x-large snakes",
+                            "medium density,\n more small snakes",
+                            "medium density,\n more x-large snakes",
+                            "high density,\n more small snakes",
+                            "high density,\n more x-large snakes")
+names(plot_labels$permutation) <- permutations
 
+plot_colors <- list()
+plot_colors$strategy <- hue_pal()(length(strategies))
+names(plot_colors$strategy) <- strategies
+plot_colors$permutation <- hue_pal()(length(permutations))
+names(plot_colors$permutation) <- permutations
 
 # Checking for and creating if necessary all results folders
-strategies <- paste0("Strategy_", c("one", "two", "three", "four"))
 save_folder <- paste0(here("Results", "alt_strategies"), "/")
 
-#strategy_name <- strategies[3]
-for(strategy_name in strategies) {
-  permutation_N_data_list <- list()
-  permutation_estimates_list <- list()
-  permutation_conditions_list <- list()
-  permutation_objectives_probs <- as.data.frame(matrix(NA, 
-                                                          nrow = (length(starting_pop)*length(starting_size_dist)),
-                                                          ncol = 8))
-  colnames(permutation_objectives_probs) <- c("init_pop", "init_size_dist", "erad_prob", "mean_erad_quarter",
-                                              "total_suppress_prob", "mean_total_quarter",  "upper_3_suppress_prob",
-                                              "mean_upper_3_quarter")
-  row_counter <- 1
-  for(P in names(starting_pop)) {
-    permutation_N_data_list[[P]] <- list()
-    permutation_estimates_list[[P]] <- list()
-    permutation_conditions_list[[P]] <- list()
-    #permutation_erad_prob_list[[P]] <- list()
-    for(D in names(starting_size_dist)) {
-      permutation_results <- readRDS(paste0(here("Results", "alt_strategies", strategy_name, "permutation_results"),
-                                            "/permutation-", P, "_", D, "_results.RDS"))
-      # Assign N data 
-      permutation_N_data_list[[P]][[D]] <- permutation_results$N_data_plot$data_all_variants
-      permutation_N_data_list[[P]][[D]]$init_size_dist <- D
-      permutation_N_data_list[[P]][[D]]$init_pop <- P
-      # Assign estimated N data
-      permutation_estimates_list[[P]][[D]] <- permutation_results$results_v_data_plot$melted_variant_results
-      permutation_estimates_list[[P]][[D]]$init_size_dist <- D
-      permutation_estimates_list[[P]][[D]]$init_pop <- P
-      # Assign eradication probability 
-      #permutation_erad_prob_list[[P]][[D]] <- permutation_results$erad_prob
-      
-      ##### COME BACK TO THIS - HAVE TO RECALCULATE (quarter when eradication occurs) FROM ORIGINAL DATA #####
-      # # Save the probability of meeting each of the 3 objectives
-      # permutation_objectives_probs$init_pop[row_counter] <- P
-      # permutation_objectives_probs$init_size_dist[row_counter] <- D
-      # permutation_objectives_probs$erad_prob[row_counter] <- permutation_results$erad_prob
-      # permutation_objectives_probs$total_suppress_prob[row_counter] <- permutation_results$total_suppress_prob$suppession_prob
-      # permutation_objectives_probs$upper_3_suppress_prob[row_counter] <- permutation_results$upper_3_suppress_prob$suppession_prob
-      # # Calculate the mean quarter when the permutation reached an objective, if it did
-      # if(permutation_objectives_probs$erad_prob[row_counter] > 0) {
-      #   # First, calculate the number of quarters each variant reached
-      #   total_quarters <- vector()
-      #   for(variant in 1:num_variants) {
-      #     variant_data <- permutation_N_data_list[[P]][[D]][permutation_N_data_list[[P]][[D]]$variant == variant,]
-      #     total_quarters[variant] <- max(variant_data$Quarter)
-      #   }
-      #   # Separate out 
-      #   erad_quarter <- total_quarters[total_quarters < 40]
-      #   permutation_objectives_probs$mean_erad_quarter[row_counter] <- mean(erad_quarter)
-      # } else { # if the permutation had 0 eradication probability, this is NA
-      #   permutation_objectives_probs$mean_erad_quarter[row_counter] <- NA
-      # }
-      # # If the total suppression objective probability is higher than 0, calculate the mean quarter when the population went below the goal
-      # if(permutation_objectives_probs$total_suppress_prob[row_counter] > 0) {
-      #   maintained_ind <- permutation_results$total_suppress_prob$suppression_maintained
-      #   permutation_objectives_probs$mean_total_quarter[row_counter] <- mean(permutation_results$total_suppress_prob$suppression_reached[maintained_ind])
-      # } else {
-      #   permutation_objectives_probs$mean_total_quarter[row_counter] <- NA
-      # }
-      # # Same as above for the upper 3 size class objective
-      # if(permutation_objectives_probs$upper_3_suppress_prob[row_counter] > 0) {
-      #   maintained_ind <- permutation_results$upper_3_suppress_prob$suppression_maintained
-      #   permutation_objectives_probs$mean_upper_3_quarter[row_counter] <- mean(permutation_results$upper_3_suppress_prob$suppression_reached[maintained_ind])
-      # } else {
-      #   permutation_objectives_probs$mean_upper_3_quarter[row_counter] <- NA
-      # }
-      
-      # Processing and saving permutation costs
-      permutation_costs <- dynamic_strategy_cost_calc(permutation_results,
-                                                      strategy_condition_costs,
-                                                      strategy = strategy_name)
-      
-      # Saving the permtuation's condition list
-      permutation_conditions_list[[P]][[D]] <- permutation_results$condition_plot$condition_record
-    }
-  }
-  # Melting all eradication probabilities for the permutations
-  # permutation_erad_prob <- melt(permutation_erad_prob_list, id.vars = NULL)
-  # colnames(permutation_erad_prob) <- c("erad_prob", "init_size_dist", "init_pop")
-  
-  # Melting condition lists into a single dataframe
-  conditions_list <- melt(permutation_conditions_list, id.vars = colnames(permutation_conditions_list[[1]][[1]]))
-  colnames(conditions_list)[c(4:5)] <- c("size_dist", "starting_pop")
-  # Combining all N data with appropriate permutation labeling
-  data_list <- list()
-  for(P in 1:length(starting_pop)) {
-    data_list[[P]] <- bind_rows(permutation_N_data_list[[P]])
-  }
-  all_data <- bind_rows(data_list)
-  
-  # Creating another column so geom_path will isolate each variant in each permutation
-  all_data <- mutate(all_data, variant_pop_size_dist = paste0(init_pop, "_", 
-                                                              init_size_dist, "-",
-                                                              variant))
-  # Manually set levels for initial pop for legend order
-  all_data$init_pop <- factor(all_data$init_pop, levels = c("low", "medium", "high"))
-  
-  strategy_plot <- ggplot(all_data, aes(x = Quarter, y = N, color = init_pop)) +
-    geom_path(aes(group = variant_pop_size_dist)) +
-    #geom_point(show.legend = FALSE) +
-    facet_grid(init_size_dist ~ size_class, scales = "free_y",
-               labeller = labeller(size_class = plot_labels$type_of_N)) +
-    # scale_color_manual(values = colors$alt,
-    #                    labels = plot_labels$alternative, 
-    #                    guide = guide_legend(position = "bottom")) +
-    labs(y = "Population", color = "", title = strategy_name) +
-    theme_bw() +
-    theme(legend.position = "top")
-  
-  ggsave(filename = paste0(save_folder, strategy_name, "/permutation_results/", strategy_name, "_permutations_data_plot.png"), 
-         strategy_plot, device = 'png', width = 6, height = 4)
-}
-
-
-
-
-### By permutation
-permutations <- vector()
-counter <- 1
-for(p in 1:length(starting_pop)) {
-  for(d in 1:length(starting_size_dist)) {
-    permutations[counter] <- paste0(names(starting_pop)[p], "_", names(starting_size_dist)[d])
-    counter <- counter + 1
-  }
-}
+#### Analyzing results by permutation & strategy ####
 
 N_data_list <- list()
 estimates_list <- list()
@@ -244,7 +147,7 @@ permutations_plot <- ggplot(all_data[all_data$size_class == "total",], aes(x = Q
 ggsave(filename = paste0(save_folder, "_all_strategies_by_permutation-total_pop.png"),
        permutations_plot, device = 'png', width = 6, height = 4)
 
-# Objectives performance
+##### Objectives performance #####
 all_objs_probs <- melt(objectives_probs, id.vars = colnames(objectives_probs[[1]]))
 colnames(all_objs_probs)[8] <- "permutation"
 
@@ -274,7 +177,7 @@ objectives_plot_by_strategy <- ggplot(all_objs_probs) +
 ggsave(filename = paste0(save_folder, "_objs_comparison_by_strategy_plot.png"),
        objectives_plot_by_strategy, device = 'png', width = 6, height = 4)
 
-## Cost calculations for strategies (dynamic strategies use means across variants)
+##### Cost calculations for strategies (dynamic strategies use means across variants) #####
 indiv_cost_categories <- c("transects", erad_methods)
 
 mean_permutation_costs <- list()
@@ -355,21 +258,39 @@ for(permutation_name in permutations) {
   }
 }
 
+
+##### Calculate RMSE & PRD #####
+# - there could be an appendix of how the estimation improves as data accumulates 
+#   (so at each estimation step that I saved, 1 - 5, and then 10, and for strategy 2, 15 and 20)
+
+
+
+#### Figures & Tables ####
+
+##### Objectives vs cost plots  #####
+
 # Eradication probabilty vs cost plots
 cost_vs_erad_objs_plot_by_permutation <- ggplot(costs_vs_obj_probs) +
   geom_point(aes(y = erad_prob, x = mean_cost/1000000, color = strategy)) +
   scale_y_continuous(limits = c(0,1)) +
-  facet_wrap(vars(permutation)) +
+  facet_wrap(vars(permutation), 
+             labeller = labeller(permutation = plot_labels$permutation)) +
   theme_bw()+
+  scale_color_manual(values = plot_colors$strategy,
+                     labels = plot_labels$strategy) +
+  theme(legend.position="bottom") +
   #theme(axis.text.x = element_text(angle = 45, hjust = 0.5, vjust = 0.75)) +
-  labs(x = "Projected mean cost (dollars in millions)", y = "Probability of full eradication")
+  labs(x = "Projected mean cost (dollars in millions)", 
+       y = "Probability of full eradication",
+       color = "Strategy")
 
 ggsave(filename = paste0(save_folder, "costs_vs_erad_obj_comparison_by_permutation_plot.png"),
-       cost_vs_erad_objs_plot_by_permutation, device = 'png', width = 6, height = 4)
+       cost_vs_erad_objs_plot_by_permutation, device = 'png', width = 7, height = 4)
 
 cost_vs_erad_objs_plot_by_strategy <- ggplot(costs_vs_obj_probs) +
   geom_point(aes(y = erad_prob, x = mean_cost/1000000, color = permutation)) +
   scale_y_continuous(limits = c(-0.01,1)) +
+  
   facet_wrap(vars(strategy)) +
   theme_bw()+
   labs(x = "Projected mean cost (dollars in millions)", y = "Probability of full eradication")
@@ -381,9 +302,14 @@ ggsave(filename = paste0(save_folder, "costs_vs_erad_obj_comparison_by_strategy_
 cost_vs_upper_3_objs_plot_by_permutation <- ggplot(costs_vs_obj_probs) +
   geom_point(aes(y = upper_3_suppress_prob, x = mean_cost/1000000, color = strategy)) +
   scale_y_continuous(limits = c(0,1)) +
-  facet_wrap(vars(permutation)) +
+  facet_wrap(vars(permutation), 
+             labeller = labeller(permutation = plot_labels$permutation)) +
+  scale_color_manual(values = plot_colors$strategy,
+                     labels = plot_labels$strategy) +
   theme_bw()+
-  labs(x = "Projected mean cost (dollars in millions)", y = "Probability of suppression upper 3 size classes")
+  labs(x = "Projected mean cost (dollars in millions)", 
+       y = "Probability of suppression upper 3 size classes",
+       color = "Strategy")
 
 ggsave(filename = paste0(save_folder, "costs_vs_upper_3_obj_comparison_by_permutation_plot.png"),
        cost_vs_upper_3_objs_plot_by_permutation, device = 'png', width = 6, height = 4)
@@ -402,9 +328,14 @@ ggsave(filename = paste0(save_folder, "costs_vs_upper_3_obj_comparison_by_strate
 cost_vs_total_supp_objs_plot_by_permutation <- ggplot(costs_vs_obj_probs) +
   geom_point(aes(y = total_suppress_prob, x = mean_cost/1000000, color = strategy)) +
   scale_y_continuous(limits = c(0,1)) +
-  facet_wrap(vars(permutation)) +
+  facet_wrap(vars(permutation), 
+             labeller = labeller(permutation = plot_labels$permutation)) +
+  scale_color_manual(values = plot_colors$strategy,
+                     labels = plot_labels$strategy) +
   theme_bw()+
-  labs(x = "Projected mean cost (dollars in millions)", y = "Probability of total population suppression")
+  labs(x = "Projected mean cost (dollars in millions)", 
+       y = "Probability of total population suppression",
+       color = "Strategy")
 
 ggsave(filename = paste0(save_folder, "costs_vs_total_supp_obj_comparison_by_permutation_plot.png"),
        cost_vs_total_supp_objs_plot_by_permutation, device = 'png', width = 6, height = 4)
@@ -419,6 +350,22 @@ cost_vs_total_supp_objs_plot_by_strategy <- ggplot(costs_vs_obj_probs) +
 ggsave(filename = paste0(save_folder, "costs_vs_total_supp_obj_comparison_by_strategy_plot.png"),
        cost_vs_total_supp_objs_plot_by_strategy, device = 'png', width = 6, height = 4)
 
+
+
+#### Plots of methods & costs ####
+
+
+##### Method combinations in each threshold condition for each strategy #####
+
+
+##### Actual methods used across replicates for each dynamic strategy #####
+
+
+##### Format dataframe for a table of total costs for each strategy #####
+
+
+
+#### Plots of accuracy & precision metrics (RMSE, PRD and coverage) & dataframe for table ####
 
 
 
